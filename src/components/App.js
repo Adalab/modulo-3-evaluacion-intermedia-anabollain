ae8245e9ddcc67d28d161db632f202de3037980a
Ana Bollain <ana.bollain@gmail.com>
2022-11-28 08:44:50 +0100

    Upload React Starter Kit

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+//import {useState, useEffect} from 'react';
+//import callToApi from '../services/api';
+//import data from '../data';
+//import ls from '../services/localStorage';
+import '../styles/App.scss';
+
+function App() {
+  //STATE VARIABLES
+  //USE EFFECT
+  //EVENT FUNCTIONS
+  //RENDER FUNCTIONS
+  //RETURN
+  return (
+    <React.Fragment>
+      {/*Header*/}
+      <h1 className = "title">Hola mundo</h1>
+      {/*Main*/}
+      {/*Footer*/}
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ src/services/api.js
@@ -0,0 +1,3 @@
+const callToApi = () => {};
+
+export {callToApi};
